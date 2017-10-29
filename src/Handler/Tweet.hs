fed5b4bc1364dc5c2829330b14a998d3fb2a9fe6
romefeller <hriqueft@gmail.com>
2017-10-29 19:37:33 +0000

    scaffold rotas de tweets

--- src/Handler/Tweet.hs
+++ src/Handler/Tweet.hs
@@ -5,3 +5,22 @@
 module Handler.Tweet where
 
 import Import
+
+import Data.Text
+
+-- WEB
+
+getTweetsR :: Text -> Handler Html
+getTweetsR uident = defaultLayout [whamlet|getTweetsR|]
+
+-- API
+
+postTweetLikeR :: UserId -> TweetId -> Handler ()
+postTweetLikeR uid tid = return ()
+
+postTweetUnlikeR :: UserId -> TweetId -> Handler ()
+postTweetUnlikeR uid tid = return ()
+
+postTweetRetweetR :: UserId -> TweetId -> Handler ()
+postTweetRetweetR uid tid = return ()
+
