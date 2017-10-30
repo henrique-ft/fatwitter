8372bbd52656ed2c6ce6f849dddfe27577331b4f
romefeller <hriqueft@gmail.com>
2017-10-30 19:45:19 +0000

    início da barra de navegação, layouts e fontawesome

--- src/Helpers/Application.hs
+++ src/Helpers/Application.hs
@@ -11,5 +11,6 @@ applicationLayout :: Yesod site => WidgetT site IO () -> HandlerT site IO Html
 applicationLayout x = defaultLayout $ do
     addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"
     addStylesheetRemote "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css"
+    addStylesheetRemote "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
     $(widgetFile "layouts/application")
     x
