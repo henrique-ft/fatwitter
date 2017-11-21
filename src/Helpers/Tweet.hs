22eff1b72d50548ba06b4faa95dde92e1c5637be
romefeller <hriqueft@gmail.com>
2017-11-21 19:06:03 +0000

    função de likes by tweet id e retweets by tweet id aprimoradas para buscar em caso de retweet

--- src/Helpers/Tweet.hs
+++ src/Helpers/Tweet.hs
@@ -7,7 +7,15 @@ module Helpers.Tweet(likesByTweetId,retweetsByTweetId) where
 import Import
 
 likesByTweetId :: TweetId -> Handler [Entity TweetLike]
-likesByTweetId tweetid = runDB $ selectList [TweetLikeTweetId ==. tweetid] []
+likesByTweetId tweetid = do 
+    tweet <- runDB $ get404 tweetid
+    case (tweetParenttweetid tweet) of
+        Nothing -> runDB $ selectList [TweetLikeTweetId ==. tweetid] []
+        Just parenttweetid -> runDB $ selectList [TweetLikeTweetId ==. parenttweetid] []
 
 retweetsByTweetId :: TweetId -> Handler [Entity Tweet]
-retweetsByTweetId tweetid = runDB $ selectList [TweetParenttweetid ==. (Just tweetid)] []
+retweetsByTweetId tweetid = do 
+    tweet <- runDB $ get404 tweetid
+    case (tweetParenttweetid tweet) of
+        Nothing -> runDB $ selectList [TweetParenttweetid ==. (Just tweetid)] []
+        Just parenttweetid -> runDB $ selectList [TweetParenttweetid ==. (Just parenttweetid)] []
